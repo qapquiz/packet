1353298da6d20ffa05a361c7f7eda48827849497
Objective <phawin@digitopolisstudio.com>
2019-11-19 15:11:50 +0700

    fix: remove return index in write packet

--- reader_test.go
+++ reader_test.go
@@ -6,7 +6,7 @@ func TestReadString(t *testing.T) {
 	want := "abc"
 
 	w := NewWriter()
-	w.WriteString(want, 0)
+	w.WriteString(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadString()
@@ -20,7 +20,7 @@ func TestReadBooleanTrue(t *testing.T) {
 	want := true
 
 	w := NewWriter()
-	w.WriteBoolean(want, 0)
+	w.WriteBoolean(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadBoolean()
@@ -34,7 +34,7 @@ func TestReadBooleanFalse(t *testing.T) {
 	want := false
 
 	w := NewWriter()
-	w.WriteBoolean(want, 0)
+	w.WriteBoolean(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadBoolean()
@@ -48,7 +48,7 @@ func TestReadFloat32(t *testing.T) {
 	want := float32(20.2)
 
 	w := NewWriter()
-	w.WriteFloat32(want, 0)
+	w.WriteFloat32(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadFloat32()
@@ -62,7 +62,7 @@ func TestReadFloat64(t *testing.T) {
 	want := float64(20.2)
 
 	w := NewWriter()
-	w.WriteFloat64(want, 0)
+	w.WriteFloat64(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadFloat64()
@@ -76,7 +76,7 @@ func TestReadInt8(t *testing.T) {
 	want := int8(20)
 
 	w := NewWriter()
-	w.WriteInt8(want, 0)
+	w.WriteInt8(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadInt8()
@@ -90,7 +90,7 @@ func TestReadInt16(t *testing.T) {
 	want := int16(20)
 
 	w := NewWriter()
-	w.WriteInt16(want, 0)
+	w.WriteInt16(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadInt16()
@@ -104,7 +104,7 @@ func TestReadInt32(t *testing.T) {
 	want := int32(20)
 
 	w := NewWriter()
-	w.WriteInt32(want, 0)
+	w.WriteInt32(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadInt32()
@@ -118,7 +118,7 @@ func TestReadInt64(t *testing.T) {
 	want := int64(20)
 
 	w := NewWriter()
-	w.WriteInt64(want, 0)
+	w.WriteInt64(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadInt64()
@@ -132,7 +132,7 @@ func TestReadUInt8(t *testing.T) {
 	want := uint8(20)
 
 	w := NewWriter()
-	w.WriteUInt8(want, 0)
+	w.WriteUInt8(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadUInt8()
@@ -146,7 +146,7 @@ func TestReadUInt16(t *testing.T) {
 	want := uint16(20)
 
 	w := NewWriter()
-	w.WriteUInt16(want, 0)
+	w.WriteUInt16(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadUInt16()
@@ -160,7 +160,7 @@ func TestReadUInt32(t *testing.T) {
 	want := uint32(20)
 
 	w := NewWriter()
-	w.WriteUInt32(want, 0)
+	w.WriteUInt32(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadUInt32()
@@ -174,7 +174,7 @@ func TestReadUInt64(t *testing.T) {
 	want := uint64(20)
 
 	w := NewWriter()
-	w.WriteUInt64(want, 0)
+	w.WriteUInt64(want)
 
 	r := NewReader(w.Bytes())
 	got := r.ReadUInt64()
